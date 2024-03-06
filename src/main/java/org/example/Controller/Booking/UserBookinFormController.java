b276eb73fad3fccfa0f1a539fd3a65608a1bd943
kavindu0818 <kavindumaduranga184@gmail.com>
2024-03-06 12:51:41 +0530

    commite

---
+++ src/main/java/org/example/Controller/Booking/UserBookinFormController.java
@@ -0,0 +1,4 @@
+package org.example.Controller.Booking;
+
+public class UserBookinFormController {
+}
--- src/main/java/org/example/Controller/dashboard/UserDashboardFormController.java
+++ src/main/java/org/example/Controller/dashboard/UserDashboardFormController.java
@@ -1,4 +1,20 @@
 package org.example.Controller.dashboard;
 
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+
 public class UserDashboardFormController {
+    public AnchorPane ancMain;
+
+    public void btnBookinOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnHistoryOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnBranchDetailsOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAllBookDetailsOnAction(ActionEvent actionEvent) {
+    }
 }
