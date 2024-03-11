bdd6118286f7807e850759624cd1f720f9dc31ed
kavindu0818 <kavindumaduranga184@gmail.com>
2024-03-12 00:14:19 +0530

    commite

--- src/main/java/BorrowingHistoryFromController.java
+++ src/main/java/BorrowingHistoryFromController.java
@@ -1,2 +1,20 @@
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class BorrowingHistoryFromController {
+    public AnchorPane ancBookingHistory;
+    public TableView tblBookingHisory;
+    public TableColumn colBookId;
+    public TableColumn colBookTitle;
+    public TableColumn colBookingDate;
+    public TableColumn colHandOverDate;
+    public TableColumn colCustomerId;
+    public TableColumn colCusstomerName;
+    public TextField txtSearchBook;
+
+    public void btnSearchBookOnAction(ActionEvent actionEvent) {
+    }
 }
